073bb7298cf719e03493b22fc61f5e78fea00d8d
willson.melo <willson.melo@payulatam.com>
2021-03-06 17:32:43 -0500

    Iterating with Streams

--- src/test/java/lectures/Lecture2.java
+++ src/test/java/lectures/Lecture2.java
@@ -1,49 +1,46 @@
 package lectures;
 
 import beans.Person;
+
 import java.util.List;
 import java.util.stream.IntStream;
+
 import mockdata.MockData;
 import org.junit.Test;
 
 public class Lecture2 {
 
-  @Test
-  public void range() throws Exception {
+    @Test
+    public void range() throws Exception {
 
-    System.out.println("for i");
+        for (int i = 0; i <= 10 ; i++) {
+            System.out.println(i);
+        }
 
-    for (int i = 0; i <= 10; i++) {
-      System.out.println(i);
+        System.out.println();
+
+        IntStream.range(0,11)
+                .forEach(System.out::println);
+
     }
 
-    System.out.println("exclusive");
+    @Test
+    public void rangeIteratingLists() throws Exception {
+        List<Person> people = MockData.getPeople();
 
-    IntStream.range(0, 10).forEach(System.out::println);
+        IntStream.range(0, people.size())
+                .forEach(index -> {
+                    Person person = people.get(index);
+                    System.out.println(person);
+                });
 
-    System.out.println("inclusive");
+    }
 
-    IntStream.rangeClosed(0, 10).forEach(System.out::println);
-
-  }
-
-  @Test
-  public void rangeIteratingLists() throws Exception {
-    List<Person> people = MockData.getPeople();
-
-    IntStream.range(0, people.size())
-        .forEach(index -> {
-          Person person = people.get(index);
-          System.out.println(person);
-        });
-
-  }
-
-  @Test
-  public void intStreamIterate() throws Exception {
-      IntStream.iterate(0, operand -> operand + 1)
-          .filter(number -> number % 2 == 0)
-          .limit(20)
-          .forEach(System.out::println);
-  }
+    @Test
+    public void intStreamIterate() throws Exception {
+        IntStream.iterate(0, operand -> operand + 1)
+                .filter(value -> value % 2 == 0)
+                .limit(20)
+                .forEach(System.out::println);
+    }
 }
