0e104d42d0ec9640f8cab6a37892b248b06d136b
willson.melo <willson.melo@payulatam.com>
2021-03-06 18:14:59 -0500

    Filtering and Trasnformations

--- src/test/java/lectures/Lecture5.java
+++ src/test/java/lectures/Lecture5.java
@@ -7,60 +7,62 @@ import beans.Car;
 import beans.Person;
 import beans.PersonDTO;
 import com.google.common.collect.ImmutableList;
+
 import java.util.List;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
+
 import mockdata.MockData;
 import org.junit.Test;
 
 public class Lecture5 {
 
-  @Test
-  public void understandingFilter() throws Exception {
-    ImmutableList<Car> cars = MockData.getCars();
+    @Test
+    public void understandingFilter() throws Exception {
+        ImmutableList<Car> cars = MockData.getCars();
 
-    final Predicate<Car> carPredicate = car -> car.getPrice() < 20000;
+        List<Car> carsFiltered = cars.stream()
+                .filter(car -> car.getPrice() < 10000)
+                .collect(Collectors.toList());
 
-    List<Car> carsFiltered = cars.stream()
-        .filter(carPredicate)
-        .collect(Collectors.toList());
-
-    carsFiltered.forEach(System.out::println);
-    System.out.println(carsFiltered.size());
-  }
+        carsFiltered.forEach(System.out::println);
+        System.out.println(carsFiltered.size());
+    }
 
 
-  @Test
-  public void ourFirstMapping() throws Exception {
-    // transform from one data type to another
-    List<Person> people = MockData.getPeople();
+    @Test
+    public void ourFirstMapping() throws Exception {
+        // transform from one data type to another
+        List<Person> people = MockData.getPeople();
 
-    List<PersonDTO> dtos = people.stream()
-        .map(PersonDTO::map)
-        .collect(Collectors.toList());
+        List<PersonDTO> peopleDto = people.stream()
+                .map(PersonDTO::map)
+                .collect(Collectors.toList());
 
-    dtos.forEach(System.out::println);
+        //peopleDto.forEach(System.out::println);
+        assertThat(peopleDto.size()).isEqualTo(people.size());
 
-    assertThat(dtos).hasSize(1000);
-    System.out.println(dtos.size());
 
-  }
+    }
 
-  @Test
-  public void averageCarPrice() throws Exception {
-    // calculate average of car prices
-    double average = MockData.getCars()
-        .stream()
-        .mapToDouble(Car::getPrice)
-        .average()
-        .orElse(0);
-    System.out.println(average);
-  }
+    @Test
+    public void averageCarPrice() throws Exception {
+        // calculate average of car prices
+        ImmutableList<Car> cars = MockData.getCars();
 
-  @Test
-  public void test() throws Exception {
-    MockData.getCars().forEach(System.out::println);
-  }
+        double carsPriceAverage = cars.stream()
+                .mapToDouble(Car::getPrice)
+                .average()
+                .orElse(0);
+
+        System.out.println(carsPriceAverage);
+
+    }
+
+    @Test
+    public void test() throws Exception {
+        MockData.getCars().forEach(System.out::println);
+    }
 }
 
 
