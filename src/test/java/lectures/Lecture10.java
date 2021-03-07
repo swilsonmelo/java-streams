e6066b1d8e6a9249c730844480b29f0531455ebf
willson.melo <willson.melo@payulatam.com>
2021-03-06 19:21:10 -0500

    Reduce and  flatMap

--- src/test/java/lectures/Lecture10.java
+++ src/test/java/lectures/Lecture10.java
@@ -2,52 +2,54 @@ package lectures;
 
 
 import com.google.common.collect.Lists;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
+
 import org.junit.Before;
 import org.junit.Test;
 
 public class Lecture10 {
 
-  private static final List<ArrayList<String>> arrayListOfNames = Lists.newArrayList(
-      Lists.newArrayList("Mariam", "Alex", "Ismail"),
-      Lists.newArrayList("John", "Alesha", "Andre"),
-      Lists.newArrayList("Susy", "Ali")
-  );
+    private static final List<ArrayList<String>> arrayListOfNames = Lists.newArrayList(
+            Lists.newArrayList("Mariam", "Alex", "Ismail"),
+            Lists.newArrayList("John", "Alesha", "Andre"),
+            Lists.newArrayList("Susy", "Ali")
+    );
 
-  @Before
-  public void setUp() {
-    System.out.println(arrayListOfNames);
-  }
-
-  @Test
-  public void withoutFlatMap() throws Exception {
-//    [Mariam, Alex, Ismail, John, Alesha, Andre, Susy, Ali]
-
-    List<String> names = Lists.newArrayList();
-
-    for(List<String> listOfNames : arrayListOfNames) {
-      for(String name : listOfNames) {
-        names.add(name);
-      }
+    @Before
+    public void setUp() {
+        System.out.println(arrayListOfNames);
     }
 
-    System.out.println(names);
+    @Test
+    public void withoutFlatMap() throws Exception {
+//    [Mariam, Alex, Ismail, John, Alesha, Andre, Susy, Ali]
 
-  }
+        List<String> names = Lists.newArrayList();
 
-  @Test
-  public void withFlatMap() throws Exception {
+        for (ArrayList<String> stringArrayList : arrayListOfNames) {
+            for (String name : stringArrayList) {
+                names.add(name);
+            }
+        }
+
+        System.out.println(names);
+
+
+    }
+
+    @Test
+    public void withFlatMap() throws Exception {
 //   [Mariam, Alex, Ismail, John, Alesha, Andre, Susy, Ali]
 
-    List<String> names = arrayListOfNames.stream()
-        .flatMap(List::stream)
-        .collect(Collectors.toList());
+        List<String> names = arrayListOfNames.stream()
+                .flatMap(List::stream)
+                .collect(Collectors.toList());
 
-    System.out.println(names);
-
-  }
+        System.out.println(names);
+    }
 
 }
 
